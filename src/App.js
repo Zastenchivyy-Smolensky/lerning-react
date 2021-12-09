439caa4047ec2167aaa6e2685a0f5d1e55f855b2
ryouta <89526043+Zastenchivyy-Smolensky@users.noreply.github.com>
2021-12-09 12:37:27 +0900

    finish eizokuteki

--- src/App.js
+++ src/App.js
@@ -2,68 +2,59 @@ import "./App.css"
 
 import React,{useEffect, useState} from "react"
 
-const total =(a)=>{
-  let re=0
-  for(let i=0;i<=a;i++){
-    re+=i
-  }
-  return re
-}
-const tax=(a)=>{
-  return Math.floor(a*1.1)
-}
-function useCalc(num=0,func=(a)=>{return a}){
-  const [msg,setMsg]=useState(null)
-  const setValue=(p)=>{
-    let res=func(p)
-    setMsg(<p className="h5">※{p}の結果は,{res}です.</p>)
-  }
-  return [msg,setValue]
-}
-function PlainMessage(props){
-  const [msg,setCalc]=useCalc()
-  const onChange=(e)=>{
-    setCalc(e.target.value)
-  }
-  return <div className="p-3 h5">
-    <h5>{msg}</h5>
-    <input type="number" onChange={onChange} className="form-control" />
-  </div>
-}
-function AlertMessage(props){
-  const [msg,setCalc]=useCalc(0,total)
-  const onChange=(e)=>{
-    setCalc(e.target.value)
-  }
-  return <div className="alert alert-primary h5 text-primary">
-    <h5>{msg}</h5>
-    <input type="submit" onChange={onChange} min="0" max="1000" className="fomr-control" />
-  </div>
-}
-function CardMessage(props){
-  const [msg,setCalc]=useCalc(0,tax)
-  const onChange=(e)=>{
-    setCalc(e.target.value)
-  }
-  return <div className="card p-3 h5 border-primary">
-    <h5>{msg}</h5>
-    <input type="range" onChange={onChange}
-    min="0" max="10000" step="100" className="form-control" />
-    </div>
-}
+import usePersist from "./Persist"
 
+function AlertMessage(props){
+  const [name,setName]=useState("")
+  const [mail,setMail]=useState("")
+  const [age,setAge]=useState(0)
+  const [mydata,setMydata]=usePersist("mydata",null)
+
+  const onChangeName=(e)=>{
+    setName(e.target.value)
+  }
+  const onChangeMail=(e)=>{
+    setMail(e.target.value)
+  }
+  const onChangeAge=(e)=>{
+    setAge(e.target.value)
+  }
+  const onAction=(e)=>{
+    const data={
+      name:name,
+      mail:mail,
+      age:age
+    }
+    setMydata(data)
+  }
+  return <div className="alert alert-primary h5 text-white">
+    <h5 className="mb-4">{JSON.stringify(mydata)}</h5>
+    <div className="form-group">
+      <label className="h6">Name:</label>
+      <input type="text" onChange={onChangeName} className="form-control" />
+    </div>
+    <div className="form-group">
+      <label className="h6">Mail</label>
+      <input type="mail" onChange={onChangeMail} className="form-control" />
+    </div>
+    <div className="form-group">
+      <label className="h6">Age</label>
+      <input type="number" className="form-control" onChange={onChangeAge} />
+    </div>
+    <button className="btn btn-primary" onClick={onAction}>Save it</button>
+    </div>
+    
+  }
   function App(){
     return(
       <div>
         <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
-          <h4 className="my03">Hooks sample</h4>
-          <PlainMessage/>
-          <AlertMessage/>
-          <CardMessage/>
-          </div>
+          <h4 className="my-3">Hooks sample</h4>
+            <AlertMessage/>
+          
         </div>
-    )
-  }
-
+      </div>
+  )
+}
 export default App
